eae610a89a5fd29cc9ce95ecd310b2f86b2679b1
KrupDat2020 <mark.krupinsky@gmail.com>
2021-04-09 09:32:03 -0500

    first commit of Sprint Challenge

--- src/components/header.js
+++ src/components/header.js
@@ -11,6 +11,33 @@ const Header = (title, date, temp) => {
   //    <span class="temp">{ temp }</span>
   //  </div>
   //
+function myHeader(title, date, temp) {
+  const headerDiv = document.createElement('div');
+  const dateSpan = document.createElement('class');
+  const h1Title = document.createElement('h1');
+  const tempSpan = document.createElement('class');
+
+ headerDiv.classList.add('header');
+ dateSpan.classList.add('date');
+ h1Title.classList.add('title');
+ tempSpan.classlist.add('temp');
+
+ headerDiv.appendChild(dateSpan);
+ headerDiv.appendChild(h1Title);
+ headerDiv.appendChild(tempSpan);
+
+ dateSpan.textContent = date;
+ h1Title.textContent = title;
+ tempSpan.textContent = temp;
+
+ console.log(myHeader)
+ return myHeader;
+}
+
+
+
+
+
 }
 
 const headerAppender = (selector) => {
@@ -20,6 +47,12 @@ const headerAppender = (selector) => {
   // It should create a header using the Header component above, passing arguments of your choosing.
   // It should append the header to the element in the DOM that matches the given selector.
   //
+  function myAppender(selector) {
+    const newHeaderAppender = document.querySelector('headerDiv');
+    const newDateSpan = document.querySelector('dateSpan');
+    const newH1Title = document.querySelector.apply('h1Title');
+    const newTempSpan = document.querySelector('tempSpan');
+  }
 }
 
 export { Header, headerAppender }
